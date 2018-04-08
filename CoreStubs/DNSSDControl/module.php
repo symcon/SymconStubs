c6dfc2fcd234267923762dba30975e0e26d916e8
Michael Maroszek <paresy@gmail.com>
2018-04-08 22:09:26 +0000

    Apply fixes from StyleCI

--- CoreStubs/DNSSDControl/module.php
+++ CoreStubs/DNSSDControl/module.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 class DNSSDControl extends IPSModule
 {
-
     public function Create()
     {
         parent::Create();
 
         $this->RegisterPropertyString('Services', '[]');
     }
-
 }
