c1b55f63b5aa43d2e34ec080b327ba1ace9de915
Brovning <39644775+Brovning@users.noreply.github.com>
2019-12-14 08:56:42 +0100

    tried to add ModbusGateway
    Hello,
    I'm struggled with adding a ModbusGateway to test my module.
    How can I stub a ModbusGateway?
    Who is able to help here?

---
+++ IOStubs/ModbusGateway/module.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/../VirtualIO/module.php';
+
+class ModBusGateway extends VirtualIO
+{
+    public function Create()
+    {
+        parent::Create();
+
+        // "GatewayMode" (0: "Modbus TCP", 1: "Modbus RTU", 2: "Modbus RTU over TCP", 3: "Modbus TCP over UDP", 4: "SymBox with RS485 (Modbus RTU)")
+        $this->RegisterPropertyInteger('GatewayMode', 0);
+        $this->RegisterPropertyInteger('DeviceID', 1);
+        // "Swap LSW/MSW for 32Bit/64Bit values"
+        $this->RegisterPropertyBoolean('SwapWords', false);
+    }
+}
