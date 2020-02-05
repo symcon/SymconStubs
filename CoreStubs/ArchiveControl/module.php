227b50b12563622616a6abd284d8637f7afa631a
Till Brede <tb@symcon.de>
2020-02-05 09:35:02 +0100

    added some functionality from ArchiveControl Module for testing

---
+++ CoreStubs/ArchiveControl/module.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+class ArchiveControl extends IPSModule
+{
+    private $Archive = [];
+
+    public function Create()
+    {
+        parent::Create();
+    }
+
+    private function GetVariableData($VariableID)
+    {
+        if (!isset($this->Archive[$VariableID])) {
+            $this->Archive[$VariableID] = [
+                'Logged' => false,
+                'Data'   => []
+            ];
+        }
+
+        return $this->Archive[$VariableID];
+    }
+
+    private function SetVariableData($VariableID, $Data)
+    {
+        $this->Archive[$VariableID] = $Data;
+    }
+
+    public function SetLoggingStatus(int $VariableID, bool $Active)
+    {
+        $data = $this->GetVariableData($VariableID);
+        $data['Logged'] = $Active;
+        $this->SetVariableData($VariableID, $data);
+    }
+
+    public function GetLoggingStatus(int $VariableID)
+    {
+        return $this->GetVariableData($VariableID)['Logged'];
+    }
+
+    public function AddLoggedValues(int $VariableID, array $NewData)
+    {
+        $ArchivedData = $this->GetVariableData($VariableID);
+        foreach ($NewData as $dataset) {
+            $ArchivedData['Data'][] = $dataset;
+        }
+        $this->SetVariableData($VariableID, $ArchivedData);
+    }
+
+    public function GetLoggedValues(int $VariableID, int $StartTime, int $EndTime, int $Limit = 10000)
+    {
+        if ($Limit > 10000 || $Limit == 0) {
+            $Limit = 10000;
+        }
+        $ArchivedData = $this->GetVariableData($VariableID);
+        $return = [];
+        foreach ($ArchivedData['Data'] as $data) {
+            if (count($return) < $Limit) {
+                if (($data['TimeStamp'] >= $StartTime) && ($data['TimeStamp'] <= $EndTime)) {
+                    $return[] = $data;
+                }
+            } else {
+                return array_reverse($return);
+            }
+        }
+        return array_reverse($return);
+    }
+}
