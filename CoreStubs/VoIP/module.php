cc1d77cf1da350f29ad2d70d46e26b35d5ab1f52
Till Brede <tb@symcon.de>
2020-09-04 16:54:06 +0200

    fixed style

--- CoreStubs/VoIP/module.php
+++ CoreStubs/VoIP/module.php
@@ -4,36 +4,16 @@ declare(strict_types=1);
 
 class VoIP extends IPSModule
 {
-
     private $connections = [];
     private $newID = 0;
 
-    private function createCall(string $number) {
-        $id = $this->newID;
-        $this->connections[$id] = [
-            'ID' => $id,
-            'TimeStamp' => time(),
-            'Number' => $number,
-            'Direction' => 1,
-            'Connected' => false,
-            'Disconnected' => true
-        ];
-
-        //increment id
-        $this->newID++;
-        return $id;
-    }
-
-    private function setConnected(int $connectionID, bool $connected) {
-        $this->connections[$connectionID]['Connected'] = $connected;
-        $this->connections[$connectionID]['Disconnected'] = !$connected;
-    }
-
-    public function StubsGetConnections() {
+    public function StubsGetConnections()
+    {
         return $this->connections;
     }
 
-    public function StubsAnswerOutgoingCall(int $ConnectionID) {
+    public function StubsAnswerOutgoingCall(int $ConnectionID)
+    {
         $this->setConnected($ConnectionID, true);
     }
 
@@ -44,40 +24,71 @@ class VoIP extends IPSModule
         $this->RegisterPropertyInteger('ProcessingScript', 0);
     }
 
-    public function AcceptCall(int $ConnectionID) {
+    public function AcceptCall(int $ConnectionID)
+    {
         throw new Exception('Not implemented');
     }
 
-    public function Connect(string $Number) {
+    public function Connect(string $Number)
+    {
         return $this->createCall($Number);
     }
 
-    public function Disconnect(int $ConnectionID) {
+    public function Disconnect(int $ConnectionID)
+    {
         unset($this->connections[$ConnectionID]);
     }
 
-    public function GetConnection(int $ConnectionID) {
+    public function GetConnection(int $ConnectionID)
+    {
         return $this->connections[$ConnectionID];
     }
 
-    public function GetData(int $ConnectionID) {
+    public function GetData(int $ConnectionID)
+    {
         throw new Exception('Not implemented');
     }
 
-    public function PlayWave(int $ConnectionID, int $FileName) {
+    public function PlayWave(int $ConnectionID, int $FileName)
+    {
         throw new Exception('Not implemented');
     }
 
-    public function RejectCall(int $ConnectionID) {
+    public function RejectCall(int $ConnectionID)
+    {
         throw new Exception('Not implemented');
     }
 
-    public function SendDTMF(int $ConnectionID, string $DTMF) {
+    public function SendDTMF(int $ConnectionID, string $DTMF)
+    {
         throw new Exception('Not implemented');
     }
 
-    public function SetData(int $ConnectionID, string $Data) {
+    public function SetData(int $ConnectionID, string $Data)
+    {
         throw new Exception('Not implemented');
     }
 
+    private function createCall(string $number)
+    {
+        $id = $this->newID;
+        $this->connections[$id] = [
+            'ID'           => $id,
+            'TimeStamp'    => time(),
+            'Number'       => $number,
+            'Direction'    => 1,
+            'Connected'    => false,
+            'Disconnected' => true
+        ];
+
+        //increment id
+        $this->newID++;
+        return $id;
+    }
+
+    private function setConnected(int $connectionID, bool $connected)
+    {
+        $this->connections[$connectionID]['Connected'] = $connected;
+        $this->connections[$connectionID]['Disconnected'] = !$connected;
+    }
 }
