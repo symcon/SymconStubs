3747477511c93a91fd5e53d90c9098461d9bea7e
Michael Tröger <mt@neo-ami.de>
2021-01-11 10:15:02 +0100

    Add  SYS_* & UC_* Stubs (#40)

---
+++ CoreStubs/Util Control/module.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+class UtilControl extends IPSModule
+{
+    public function Create()
+    {
+        parent::Create();
+    }
+
+    public function GetLogMessageStatistics()
+    {
+        return [];
+    }
+}
