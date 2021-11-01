64c06117d2c8de6a326b6eeaaba8c41813dc06f3
Niels Thole <nt@symcon.de>
2021-11-01 16:09:12 +0100

    Add helpful autoload.php to include all at once

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/ConstantStubs.php';
+include_once __DIR__ . '/GlobalStubs.php';
+include_once __DIR__ . '/KernelStubs.php';
+include_once __DIR__ . '/ModuleStubs.php';
