bafb95f6239c8bb81c6b33202b845117a8125d51
Till Brede <tb@symcon.de>
2025-03-24 09:16:05 +0100

    add request action RequestActionEx

--- GlobalStubs.php
+++ GlobalStubs.php
@@ -221,6 +221,32 @@ function RequestAction(int $VariableID, $Value)
     }
 }
 
+function RequestActionEx(int $VariableID, $Value, $Sender)
+{
+    $v = IPS\VariableManager::getVariable($VariableID);
+    if ($v['VariableCustomAction'] > 0) {
+        $actionID = $v['VariableCustomAction'];
+    } else {
+        $actionID = $v['VariableAction'];
+    }
+    if (IPS_InstanceExists($actionID)) {
+        $o = IPS\ObjectManager::getObject($VariableID);
+        $interface = IPS\InstanceManager::getInstanceInterface($actionID);
+        $interface->RequestAction($o['ObjectIdent'], $Value);
+    } elseif (IPS_ScriptExists($actionID)) {
+        $result = IPS_RunScriptWaitEx($actionID, [
+            'VARIABLE' => $VariableID,
+            'VALUE'    => $Value,
+            'SENDER'   => $Sender,
+        ]);
+        if (strlen($result) > 0) {
+            echo $result;
+        }
+    } else {
+        throw new Exception('Action is invalid');
+    }
+}
+
 /* Object Manager */
 function IPS_SetParent(int $ID, int $ParentID)
 {
