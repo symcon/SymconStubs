443ff1170176e4b8efa30faaa9d6cfbd56afd2ef
TillBrede <51406030+TillBrede@users.noreply.github.com>
2025-09-30 17:08:04 +0200

    add visualizations stubs

---
+++ CoreStubs/Tile Visualization/module.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+class TileVisualization extends IPSModule
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+
+        // This should be in the NotificationControl
+        $this->RegisterAttributeInteger('LastNotificationsID', 0);
+    }
+
+    public function OpenObject(int $ObjectID, string $TokenList): void
+    {
+        // No need to implement this function.
+    }
+
+    public function PostNotification(string $Title, string $Text, string $Type, int $TargetID): int
+    {
+        return self::PostNotificationEx($Title, $Text, '', '', $TargetID);
+    }
+    
+    public function PostNotificationEx(string $Title, String $Text, String $Icon , String $Sound, int $TargetID): int
+    {
+        $nextNotificationID = $this->ReadAttributeInteger('LastNotificationsID') + 1;
+        $this->WriteAttributeInteger('LastNotificationsID', $nextNotificationID);
+        return $nextNotificationID;
+    }
+}
---
+++ CoreStubs/WebFront/module.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+class WebFrontVisualization extends IPSModule
+{
+    public function AudioNotification(string $Title, int $MediaID): void
+    {
+        // No need to implement this function.
+    }
+
+
+    public function OpenCategory(int $CategoryID): void
+    {
+        // No need to implement this function.
+    }
+
+
+    public function PushNotification(string $Title, string $Text, string $Sound, int $TargetID): void
+    {
+        // No need to implement this function.
+    }
+
+    public function Reload(): void
+    {
+        // No need to implement this function.
+    }
+
+
+    public function SendNotification(string $Title, string $Text, string $Icon, int $Timeout): void
+    {
+        // No need to implement this function.
+    }
+
+    public function SendPopup(string $Title, string $Text): void
+    {
+        // No need to implement this function.
+    }
+
+    public function SwitchPage(string $PageName): void
+    {
+        // No need to implement this function.
+    }
+}
